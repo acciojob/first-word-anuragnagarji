1f5a32280a2fb4f3d939705a553fe8dc0ddb3c59
anuragnagarji <142933378+anuragnagarji@users.noreply.github.com>
2023-12-30 14:38:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function firstWord(s) {
-  // your code here
+if(s.indexOf(" ")>0){
+	return s.slice(0,s.indexOf(" "));
+}
+else return s;
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
